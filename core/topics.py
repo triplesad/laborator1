b1d45d2ad61716f51ac5e6edda958eda093bcbb5
triplesad <qtyr7006@gmail.com>
2026-03-20 02:51:59 +0300

    Revert "Удалены несколько файлов проекта"
    This reverts commit 83ddad2dbc33cbb7559f741aa996ca91061b30f2.

---
+++ core/topics.py
@@ -0,0 +1,8 @@
+def create_topic(title, author):
+    print(f"Тема '{title}' создана пользователем {author}")
+
+
+def add_message(topic, author, text):
+    print(f"В тему '{topic}' добавлено сообщение от {author}: {text}")
+
+print("Модуль работы с темами форума")
